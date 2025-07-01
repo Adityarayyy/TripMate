b41a2ab27ea109fbef48fbcfa827121e2334f20b
Adityarayyy <adityasray46@gmail.com>
2025-07-01 16:38:44 +0530

    build script

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+flutter build web \
+  --dart-define=SUPABASE_URL=$SUPABASE_URL \
+  --dart-define=SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY \
+  --dart-define=GEMINI_API_KEY=$GEMINI_API_KEY
